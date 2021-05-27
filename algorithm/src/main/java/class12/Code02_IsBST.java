088f4a39a89cd5fcbbc8ddded90bca6924ac50f3
ouyangliuy <745252780@qq.com>
2021-05-27 15:23:37 +0800

    算法入门之树的递归套路深入实践

--- algorithm/src/main/java/class12/Code02_IsBST.java
+++ algorithm/src/main/java/class12/Code02_IsBST.java
@@ -1,8 +1,129 @@
 package class12;
 
+import java.util.ArrayList;
+
 /**
  * 判断一棵树是否是搜索二叉树
  */
 public class Code02_IsBST {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean isBST1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        ArrayList<Node> arr = new ArrayList<>();
+        in(head, arr);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i).value <= arr.get(i - 1).value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void in(Node head, ArrayList<Node> arr) {
+        if (head == null) {
+            return;
+        }
+        in(head.left, arr);
+        arr.add(head);
+        in(head.right, arr);
+    }
+
+    //是否是搜索二叉树的
+    public static boolean isBST2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        return process(head).isBST;
+    }
+
+    public static class Info {
+        public int max;
+        public int min;
+        public boolean isBST;
+
+        public Info(int max, int min, boolean isBST) {
+            this.max = max;
+            this.min = min;
+            this.isBST = isBST;
+        }
+    }
+
+    public static Info process(Node x) {
+        if (x == null) {
+            return null;
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        int min = x.value;
+        int max = x.value;
+        if (leftInfo != null) {
+            max = Math.max(leftInfo.max, max);
+        }
+        if (rightInfo != null) {
+            max = Math.max(rightInfo.max, max);
+        }
+
+        if (leftInfo != null) {
+            min = Math.min(leftInfo.min, min);
+        }
+        if (rightInfo != null) {
+            min = Math.min(rightInfo.min, min);
+        }
+        boolean isBST = true;
+        if (leftInfo != null && !leftInfo.isBST) {
+            isBST = false;
+        }
+        if (rightInfo != null && !rightInfo.isBST) {
+            isBST = false;
+        }
+        if (rightInfo != null && rightInfo.min <= x.value) {
+            isBST = false;
+        }
+        if (leftInfo != null && leftInfo.max >= x.value) {
+            isBST = false;
+        }
+        return new Info(max, min, isBST);
+    }
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 4;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isBST1(head) != isBST2(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
 
 }
