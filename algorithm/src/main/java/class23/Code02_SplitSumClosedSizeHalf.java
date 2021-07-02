e8d32c9a8d331b1c8def50cde098da830faf1456
ouyangliuy <745252780@qq.com>
2021-07-02 10:19:52 +0800

    算法入门之暴力递归到动态规划总结

---
+++ algorithm/src/main/java/class23/Code02_SplitSumClosedSizeHalf.java
@@ -0,0 +1,174 @@
+package class23;
+
+/**
+ * 给定一个正数数组arr，请把arr中所有的数分成两个集合
+ * 如果arr长度为偶数，两个集合包含数的个数要一样多
+ * 如果arr长度为奇数，两个集合包含数的个数必须只差一个
+ * 请尽量让两个集合的累加和接近
+ * 返回：
+ * 最接近的情况下，较小集合的累加和
+ */
+public class Code02_SplitSumClosedSizeHalf {
+
+
+    public static int right(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        if ((arr.length & 1) == 0) { // 数组是偶数长度
+            return process(arr, 0, arr.length / 2, sum / 2);
+        } else { // 奇数长度,要么取奇数长度，要么取偶数长度
+            return Math.max(process(arr, 0, arr.length / 2, sum / 2), process(arr, 0, arr.length / 2 + 1, sum / 2));
+        }
+    }
+
+    // arr[i....]自由选择，挑选的个数一定要是picks个，累加和<=rest, 离rest最近的返回
+    public static int process(int[] arr, int index, int picks, int rest) {
+        if (index == arr.length) {
+            return picks == 0 ? 0 : -1;
+        } else {
+            int p1 = process(arr, index + 1, picks, rest);
+            int p2 = 0;
+            int next = 0;
+            if (rest >= arr[index]) {
+                next = process(arr, index + 1, picks - 1, rest - arr[index]);
+            }
+            if (next != -1) {
+                p2 = arr[index] + next;
+            }
+            return Math.max(p1, p2);
+        }
+    }
+
+    public static int dp(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        int N = arr.length;
+        int M = sum / 2;
+        int K = arr.length / 2 + 1;
+        int[][][] dp = new int[N + 1][M + 1][K + 1];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= M; j++) {
+                for (int k = 0; k <= K; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        // 条件1
+        for (int rest = 0; rest <= sum; rest++) {
+            dp[N][0][rest] = 0;
+        }
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= M; rest++) {
+                for (int pick = 0; pick <= K; pick++) {
+                    int p1 = dp[index + 1][pick][rest];
+                    int p2 = 0;
+                    int next = 0;
+                    if (rest >= arr[index] && pick - 1 >= 0) {
+                        next = dp[index + 1][pick - 1][rest - arr[index]];
+                    }
+                    if (next != -1) {
+                        p2 = arr[index] + next;
+                    }
+                    dp[index][pick][rest] = Math.max(p1, p2);
+                }
+            }
+        }
+
+        if ((arr.length & 1) == 0) { // 数组是偶数长度
+            return dp[0][arr.length / 2][M];
+        } else { // 奇数长度,要么取奇数长度，要么取偶数长度
+            return Math.max(dp[0][arr.length / 2][M], dp[0][arr.length / 2 + 1][M]);
+        }
+    }
+
+    public static int dp2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        sum >>= 1;
+        int N = arr.length;
+        int M = (arr.length + 1) >> 1;
+        int[][][] dp = new int[N][M + 1][sum + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= M; j++) {
+                for (int k = 0; k <= sum; k++) {
+                    dp[i][j][k] = Integer.MIN_VALUE;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k <= sum; k++) {
+                dp[i][0][k] = 0;
+            }
+        }
+        for (int k = 0; k <= sum; k++) {
+            dp[0][1][k] = arr[0] <= k ? arr[0] : Integer.MIN_VALUE;
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j <= Math.min(i + 1, M); j++) {
+                for (int k = 0; k <= sum; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (k - arr[i] >= 0) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - 1][k - arr[i]] + arr[i]);
+                    }
+                }
+            }
+        }
+        return Math.max(dp[N - 1][M][sum], dp[N - 1][N - M][sum]);
+    }
+
+    // for test
+    public static int[] randomArray(int len, int value) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * value);
+        }
+        return arr;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue = 50;
+        int testTime = 10000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int len = (int) (Math.random() * maxLen);
+            int[] arr = randomArray(len, maxValue);
+            int ans1 = right(arr);
+            int ans2 = dp(arr);
+            int ans3 = dp2(arr);
+            if (ans1 != ans2 || ans1 != ans3) {
+                printArray(arr);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                System.out.println("Oops!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+}
