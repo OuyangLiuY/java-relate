bd1aef1ccbf1b16a26b40eb7d6675f6ac9769b73
ouyangliuy <745252780@qq.com>
2021-07-19 17:20:41 +0800

    算法入门之斐波那契数列矩阵乘法

---
+++ algorithm/src/main/java/class26/Code05_TileProblem.java
@@ -0,0 +1,22 @@
+package class26;
+
+/**
+ *  用1*2的瓷砖，把N*2的区域填满
+ *
+ * 返回铺瓷砖的方法数
+ */
+public class Code05_TileProblem {
+
+    // f() = f(n-1) + f(n-2)
+    // 第一块瓷砖，竖着放，有 f(n-1)种
+    // 第一。二块瓷砖，横着放，有f(n-2)种
+    public static int res(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int[][] base = {{1, 1}, {1, 0}};
+        int[][] re = Utils.matrixPower(base, n - 2);
+        // fn = |2,1| * |re|
+        return 2 * re[0][0] + re[1][0];
+    }
+}
