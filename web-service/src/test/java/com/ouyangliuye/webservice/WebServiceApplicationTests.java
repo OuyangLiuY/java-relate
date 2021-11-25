985d84e0a0009669eb72c0bee5d4664d93386c64
ouyangliuy <745252780@qq.com>
2021-11-25 10:02:34 +0800

    init web service

---
+++ web-service/src/test/java/com/ouyangliuye/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouyangliuye.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
