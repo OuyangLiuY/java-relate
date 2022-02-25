55484d4459ed1e2272d09910d41bd50a548cfbf2
ouyangliuy <745252780@qq.com>
2022-02-25 17:59:02 +0800

    pdf添加水印

--- java-base/src/main/java/com/ouyangliuy/wordtopdf/PdfWater.java
+++ java-base/src/main/java/com/ouyangliuy/wordtopdf/PdfWater.java
@@ -3,40 +3,51 @@ package com.ouyangliuy.wordtopdf;
 
 import com.aspose.pdf.*;
 
-import java.io.InputStream;
 
 public class PdfWater {
+     static  String pdfPath = "C:/Users/chopsticks/Desktop/xxx/dismiss-img-clear.pdf";
+    public static void add(){
+        // Initialize document object
+        Document document = new Document(pdfPath);
+
+//Add page
+        Page page = document.getPages().add();
+
+// Add text to new page
+        page.getParagraphs().add(new TextFragment("Hello World!"));
+
+// Save updated PDF
+        document.save("C:/Users/chopsticks/Desktop/xxx/HelloWorld_out.pdf");
+    }
 
     public static void main(String[] args) throws Exception{
+//        add();
+        addWater();
+    }
+
+    public static void  addWater(){
         boolean res = AsposeUtil.judgeLicense();
         System.out.println(res);
-        String pdfPath = "C:\\Users\\chopsticks\\Desktop\\xxx\\dismiss-img-w.pdf";
+        String pdfPath = "C:/Users/chopsticks/Desktop/xxx/dismiss-img-clear.pdf";
         Document pdfDocument = new Document(pdfPath);
-
-        TextStamp textStamp = new TextStamp("合同水印");
+        //TODO 汉字不支持问题
+        TextStamp textStamp = new TextStamp("仅供上汽总部离职业务");
+        // set whether stamp is background
         textStamp.setBackground(true);
-        textStamp.setXIndent(200);
+        // set origin
+        textStamp.setXIndent(100);
         textStamp.setYIndent(100);
+        // rotate stamp
         textStamp.setRotate(Rotation.None);
-        textStamp.getTextState().setFont( FontRepository.findFont("Arial"));
-        textStamp.getTextState().setFontSize(20.0F);
-        textStamp.getTextState().setFontStyle(FontStyles.Regular);
+        // set text properties
+        textStamp.getTextState().setFont(FontRepository.findFont("Arial"));
+        textStamp.getTextState().setFontSize(14.0F);
+        textStamp.getTextState().setFontStyle(FontStyles.Bold);
         textStamp.getTextState().setFontStyle(FontStyles.Italic);
         textStamp.getTextState().setForegroundColor(Color.getGray());
-
-
-        PageCollection pages = pdfDocument.getPages();
-        System.out.println(pages.size());
-        int size = pages.size();
-        for (int i = 0; i < size; i++) {
-            pages.get_Item(i + 1).addStamp(textStamp);
-//            pages.get_Item(i + 1).addStamp(textStamp1);
-//            pages.get_Item(i + 1).addStamp(textStamp2);
-        }
-
-  //      pages.get_Item(size-1).addStamp(textStamp);
-
-        pdfDocument.save("C:\\Users\\chopsticks\\Desktop\\xxx\\dismiss-img-word.pdf");
-
+        // add stamp to particular page
+        pdfDocument.getPages().get_Item(1).addStamp(textStamp);
+        // save output document
+        pdfDocument.save("C:/Users/chopsticks/Desktop/xxx/dismiss-img-result.pdf");
     }
 }
